e2bcdf9036f641f68bc7d132fbeb146b9a5515bc
Harrisonkamau <kamauharrison87@gmail.com>
2016-06-13 22:11:40 +0300

    Add more topics

--- js_basics.js
+++ js_basics.js
@@ -23,11 +23,87 @@ var say = "hello" + "world";
 
 
 //4. Conditionals
-var iceCream = 'chocolate';
+//if and else statement
+var iceCream = "chocolate";
 if (iceCream === 'chocolate') {
-  alert('Yay, I love chocolate ice cream!');    
+  alert('Yay, I love chocolate ice cream!');
 } else {
-  alert('Awwww, but chocolate is my favorite...');    
+  alert('Awwww, but chocolate is my favorite...');
 };
 
+//5. Functions
+var squareRoot = function(number){
+    return number ** 0.5;
+}
+squareRoot(number); //calling the function
 
+function multiply(num1,num2) {
+  var result = num1 * num2;
+  return result;
+}
+//using the function
+multiply(4,7);
+multiply(20,20);
+multiply(0.5,3);
+
+
+//6. Dynamic typing
+//JavaScript is a loosely typed or a dynamic language. That means you don't have to declare the type of a variable ahead of time. The type will get determined automatically while the program is being processed. That also means that you can have the same variable as different types:
+var foo = 42;    // foo is now a Number
+var foo = "bar"; // foo is now a String
+var foo = true;  // foo is now a Boolean
+
+
+//7. Data types
+    -Boolean( true and false)
+//      boolean values
+        console . log (3 > 2)
+        // → true
+    -Null (The Null type has exactly one value: null.)
+    -Undefined(A variable that has not been assigned a value has the value undefined)
+    -Number
+    -String
+    
+//8. Unary operators
+//    example unary operator
+    console.log ( typeof 4.5);
+    // → number
+
+//9. Logical operators
+//JavaScript supports three logical operators:
+    - and(&&)
+    - or(||)
+    - not(!)
+    
+//    The && operator represents logical and. It is a binary operator, and its
+//      result is true only if both the values given to it are true.
+          console . log ( true && false )
+            // → false
+          console . log ( true && true )
+            // → true
+          
+//    The || operator denotes logical or. It produces true if either of the values
+//given to it is true.
+          console . log ( false || true )
+          // → true
+          console . log ( false || false )
+          // → false
+          
+          
+//10. Ternary
+console . log ( true ? 1 : 2) ;
+    // → 1
+console . log ( false ? 1 : 2) ;
+// → 2
+
+//11. Automatic type conversion
+console . log (8 * null )
+// → 0
+console . log ("5" - 1)
+// → 4
+console . log ("5" + 1)
+// → 51
+console . log (" five " * 2)
+// → NaN
+console . log ( false == 0)
+// → true
