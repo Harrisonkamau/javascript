3e23857a89acc00b6f28c68480b00e5dc0fa3ae6
Harrisonkamau <kamauharrison87@gmail.com>
2016-06-13 21:30:43 +0300

    Add js_basics.js file

---
+++ js_basics.js
@@ -0,0 +1,33 @@
+//1. Variables
+//variables are declared using var keyword
+var string = "Hello World!"; //the value of the variable string is Hello World!
+var greeting = 'hey';
+//You can retrieve the value by just calling the variable by name
+//console.log(string);
+
+
+//2. Comments
+//You can put comments into JavaScript code, just like you can in CSS:
+/*
+Everything in between is a comment.
+*/
+
+//If your comment contains no line breaks, it's often easier to put it behind two slashes like this:
+// This is a comment
+
+
+//3. Operators
+var total = 6 + 9;
+var subtract = 29 - 10;
+var say = "hello" + "world";
+
+
+//4. Conditionals
+var iceCream = 'chocolate';
+if (iceCream === 'chocolate') {
+  alert('Yay, I love chocolate ice cream!');    
+} else {
+  alert('Awwww, but chocolate is my favorite...');    
+};
+
+
